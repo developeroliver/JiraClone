d014a5610fb08de8068164374a9a888b119c4e87
codewitholiver <olivier.codes@gmail.com>
2025-04-17 17:58:08 +0200

    Initial Commit

---
+++ JiraClone/JiraCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  JiraCloneApp.swift
+//  JiraClone
+//
+//  Created by olivier geiger on 17/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct JiraCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
